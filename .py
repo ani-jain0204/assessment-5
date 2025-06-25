be2b1ae4403f0bdd74f6f43a75cb6aa085d50393
ani-jain0204 <anishka.jain_cs.aiml24@gla.ac.in>
2025-06-25 23:00:57 +0530

    Update .py

--- .py
+++ .py
@@ -1,8 +1,18 @@
 # assessment-5
+#Task-1
 a={"Aman":98,"piyush":76,"dev":89}
 n=input("Enter sudent name")
 if n in a:
     print(a[n])
 else:
     print("sudent name not found")
+
+#Task-2
+a= list(range(1, 11))
+b = a[:5]
+c= b[::-1]
+print("Orignal list:", a)
+
+print("First five elements:", b)
+print("Reversed first five elements:", c)
 
